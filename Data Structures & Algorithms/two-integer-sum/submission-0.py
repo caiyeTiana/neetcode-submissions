faedbc42c621fe152b3a9a2794e2b83de540ac84
Yetian Cai <149084420+caiyeTiana@users.noreply.github.com>
2026-08-09 15:31:43 -0400

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.py
@@ -0,0 +1,16 @@
+# neetcode solution 3 hash map (two pass)
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        indices = {} # val -> index
+
+        for i, n in enumerate(nums):
+            indices[n] = i
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in indices and indices[diff] != i:
+                return [i, indices[diff]]
+        return []
+
+        
