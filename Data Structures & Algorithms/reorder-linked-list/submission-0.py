615c28495ec21557d649359fc53ad685aaec7b91
Yetian Cai <149084420+caiyeTiana@users.noreply.github.com>
2026-08-12 15:22:08 -0400

    Add: reorder-linked-list - submission-0

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-0.py
@@ -0,0 +1,30 @@
+# NeetCode solution 3 -- reverse and merge (same as vedio)
+# 完全迷失了，只大概看懂了如何用two pointers找middle，后续怎么reroder完全看不懂
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        second = slow.next
+        prev = slow.next = None # second的第一个数？
+        while second:
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
+
+        first, second = head, prev
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first, second = tmp1, tmp2
